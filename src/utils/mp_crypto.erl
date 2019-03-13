49675dedead7ea411ccaf164c7852226dd95698b
Witeman Zheng <amos@gzemt.com>
2019-03-13 15:44:02 +0700

    change to cbc256 encryption

--- src/utils/mp_crypto.erl
+++ src/utils/mp_crypto.erl
@@ -13,13 +13,13 @@ encrypt(Key, Binary) ->
 
     FinalBinary = <<BinaryLength:32/integer-big, Binary/binary, 0:AdditionalLength/unit:8>>,
 
-    crypto:block_encrypt(aes_cbc128, Key, ?IV, FinalBinary).
+    crypto:block_encrypt(aes_cbc256, Key, ?IV, FinalBinary).
 
 
 -spec decrypt(nonempty_string(), binary()) -> {ok, binary()} |
                                                {error, term()}.
 decrypt(Key, Binary) ->
-    Data = crypto:block_decrypt(aes_cbc128, Key, ?IV, Binary),
+    Data = crypto:block_decrypt(aes_cbc256, Key, ?IV, Binary),
     try
         <<Length:32/integer-big, RealData:Length/binary, _Rest/binary>> = Data,
         {ok, RealData}
