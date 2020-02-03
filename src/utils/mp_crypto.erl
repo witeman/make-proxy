9642bba783279f52b3df8ea6f22f2e4ec08090ac
Amos <amos@gzemt.com>
2020-02-04 01:37:48 +0800

    change to chacha_poly1305

--- src/utils/mp_crypto.erl
+++ src/utils/mp_crypto.erl
@@ -2,26 +2,21 @@
 -export([encrypt/2,
          decrypt/2]).
 
--define(DATALENGTH, 16).
--define(IV, <<"^de$@#56*sxdfrtg">>).
+-define(TAGLENGTH, 16).
+-define(IV, <<2020020211223344:64>>).
+-define(ADD, <<"jflkadsj^de$@#56*sxdfrtg">>).
 
 -spec encrypt(nonempty_string(), binary()) -> binary().
 encrypt(Key, Binary) ->
-    BinaryLength = byte_size(Binary),
-    Rem = (BinaryLength + 4) rem ?DATALENGTH,
-    AdditionalLength = ?DATALENGTH - Rem,
-
-    FinalBinary = <<BinaryLength:32/integer-big, Binary/binary, 0:AdditionalLength/unit:8>>,
-
-    crypto:block_encrypt(aes_cbc256, Key, ?IV, FinalBinary).
-
+    {B,T} = crypto:crypto_one_time_aead(chacha20_poly1305, Key, ?IV, Binary, ?ADD, ?TAGLENGTH, true),
+    erlang:term_to_binary({B,T}).
 
 -spec decrypt(nonempty_string(), binary()) -> {ok, binary()} |
                                                {error, term()}.
 decrypt(Key, Binary) ->
-    Data = crypto:block_decrypt(aes_cbc256, Key, ?IV, Binary),
     try
-        <<Length:32/integer-big, RealData:Length/binary, _Rest/binary>> = Data,
+        {B, T} = erlang:binary_to_term(Binary),
+        RealData = crypto:crypto_one_time_aead(chacha20_poly1305, Key, ?IV, B, ?ADD, T, false),
         {ok, RealData}
     catch
         Error:Reason ->
